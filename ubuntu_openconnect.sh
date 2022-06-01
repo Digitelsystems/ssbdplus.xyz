464813594ee8fd12abeaca72beac103665e37df6
Digitelsystems <103522787+Digitelsystems@users.noreply.github.com>
2022-06-02 01:41:16 +0600

    Update ubuntu_openconnect.sh

--- ubuntu_openconnect.sh
+++ ubuntu_openconnect.sh
@@ -2,10 +2,10 @@
 cp /usr/share/zoneinfo/Asia/Dubai /etc/localtime
 
 #Database Details
-db_host='173.82.65.242';
-db_user='ssbdplus_ssbdplus';
-db_pass='digi##0550013763';
-db_name='ssbdplus_ssbdplus';
+db_host='66.45.248.110';
+db_user='digitelp_ssbdplus';
+db_pass='ME2HE2Di@';
+db_name='digitelp_ssbdplus';
 
 install_require()
 {
